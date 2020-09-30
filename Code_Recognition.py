28c452680e4ca7ff9c41c04295c4f6f9725b0df4
amunozaguilar <66769765+amunozaguilar@users.noreply.github.com>
2020-09-30 01:22:19 -0300

    Update Code_Recognition.py

--- Code_Recognition.py
+++ Code_Recognition.py
@@ -2,18 +2,22 @@
 """
 @author: Alex
 """
+# Se recomienda el uso de Colab Google para ejecutar por etapas
 # Habilitando el entorno de trabajo de Google Drive, en esta etapa nos solicitará una contraseña que debemos obtener entrando a nuestro correo
 from google.colab import drive
 drive.mount('/content/drive')
 
 # Cargar librerias
 import torch
+import PIL
 import torchvision
 from torchvision import transforms, datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn # Módulo están los modelos de red
+import torch.nn.functional as F # Módulo donde están las funciones de activación
 
 # Cargar imágenes
-import PIL
-
 data_transform = transforms.Compose([
     transforms.Resize((128,128)),  
     transforms.RandomGrayscale(p=0.2),
@@ -36,17 +40,10 @@ data_transform = transforms.Compose([
 gatos_perros_dataset = datasets.ImageFolder(root='/content/drive/My Drive/...) # ... es la ruta donde se almacena el conjunto de datos
 print(gatos_perros_dataset)
 
-dataset_loader = torch.utils.data.DataLoader(gatos_perros_dataset,
-                                             batch_size=32,
-                                             shuffle=False,
-                                             num_workers=2)
-
+dataset_loader = torch.utils.data.DataLoader(gatos_perros_dataset,batch_size=32, shuffle=False, num_workers=2)
 print(dataset_loader)
 
 # Visualización de las imágenes 
-import matplotlib.pyplot as plt
-import numpy as np
-
 plt.figure(num=None, figsize=(12, 8), dpi=80)
 
 def imshow(img):
@@ -61,65 +58,31 @@ images, labels = data_iter.next()
 imshow(torchvision.utils.make_grid(images))
 
 # Separando la base de datos
-# Transformaciones de la imagen
-import PIL
+# Transformaciones de la imagen, asi quedan mejor ajustadas
+#data_transform = transforms.Compose([transforms.Resize((32,32)), transforms.RandomGrayscale(p=0.2),transforms.RandomVerticalFlip(p=0.2),transforms.RandomHorizontalFlip(p=0.2), transforms.ToTensor()])
 
-#data_transform = transforms.Compose([
-#    transforms.Resize((32,32)),  
-#    transforms.RandomGrayscale(p=0.2),
-#    transforms.RandomVerticalFlip(p=0.2),
-#    transforms.RandomHorizontalFlip(p=0.2),                              
-#    transforms.ToTensor()
-#])
+data_transform = transforms.Compose([transforms.Resize((32,32)), transforms.RandomGrayscale(p=0.2),
+                                    transforms.RandomVerticalFlip(p=0.6), transforms.RandomHorizontalFlip(p=0.6),                              
+                                    transforms.RandomChoice([transforms.RandomAffine(degrees=0, translate=(0.1, 0.1),
+                                    scale=None, shear=None, resample=False, fillcolor=0), transforms.ColorJitter(brightness=(1, 1.5),
+                                    contrast=(0.3, 2), saturation=(0.1, 1), hue=(-0.3, 0.3)), transforms.RandomRotation((-10, 10), 
+                                    resample=PIL.Image.BILINEAR)]), transforms.ToTensor()])
 
-data_transform = transforms.Compose([
-    transforms.Resize((32,32)),  
-    transforms.RandomGrayscale(p=0.2),
-    transforms.RandomVerticalFlip(p=0.6),
-    transforms.RandomHorizontalFlip(p=0.6),                              
-    transforms.RandomChoice([
-      transforms.RandomAffine(degrees=0, translate=(0.1, 0.1),
-                          scale=None, shear=None,
-                          resample=False, fillcolor=0),
-      transforms.ColorJitter(brightness=(1, 1.5),
-                            contrast=(0.3, 2),
-                            saturation=(0.1, 1),
-                            hue=(-0.3, 0.3)),
-      transforms.RandomRotation((-10, 10), resample=PIL.Image.BILINEAR)
-    ]),
-    transforms.ToTensor()
-])
+data_transform_test = transforms.Compose([transforms.Resize((32,32)),transforms.ToTensor()])
 
-data_transform_test = transforms.Compose([
-    transforms.Resize((32,32)),                            
-    transforms.ToTensor()
-])
-
-# Cargar las imágenes
-gatos_perros_train = datasets.ImageFolder(root='/content/drive/My Drive/D-UCN/Classes/TecnicasAvanzadasAprendizajeAutomatico/Laboratorios/Laboratorio05.2:DeepLearning/dataset/training_set',
-                                          transform=data_transform)
-gatos_perros_valid = datasets.ImageFolder(root='/content/drive/My Drive/D-UCN/Classes/TecnicasAvanzadasAprendizajeAutomatico/Laboratorios/Laboratorio05.2:DeepLearning/dataset/valid_set',
-                                          transform=data_transform_test)
-gatos_perros_test = datasets.ImageFolder(root='/content/drive/My Drive/D-UCN/Classes/TecnicasAvanzadasAprendizajeAutomatico/Laboratorios/Laboratorio05.2:DeepLearning/dataset/test_set',
-                                          transform=data_transform_test)
+# Cargar las imágenes desde el acceso de las carpetas
+gatos_perros_train = datasets.ImageFolder(root='.../training_set', transform=data_transform)
+gatos_perros_valid = datasets.ImageFolder(root='.../valid_set',transform=data_transform_test)
+gatos_perros_test = datasets.ImageFolder(root='.../test_set', transform=data_transform_test)
 
 # Conjunto de entrenamiento
-train_loader = torch.utils.data.DataLoader(gatos_perros_train,
-                                           batch_size=32,
-                                           shuffle=True,
-                                           num_workers=2)
+train_loader = torch.utils.data.DataLoader(gatos_perros_train, batch_size=32, shuffle=True, num_workers=2)
 
 # Conjunto de validacion
-valid_loader = torch.utils.data.DataLoader(gatos_perros_valid,
-                                           batch_size=32,
-                                           shuffle=False,
-                                           num_workers=2)
+valid_loader = torch.utils.data.DataLoader(gatos_perros_valid, batch_size=32, shuffle=False, num_workers=2)
 
 # Conjunto de pruebas
-test_loader = torch.utils.data.DataLoader(gatos_perros_test,
-                                           batch_size=32,
-                                           shuffle=False,
-                                           num_workers=2)
+test_loader = torch.utils.data.DataLoader(gatos_perros_test, batch_size=32, shuffle=False, num_workers=2)
                                            
                                            
 # Revisión de las imágenes cargadas
@@ -132,10 +95,7 @@ images, labels = data_iter.next()
 # Mostrar imagenes
 imshow(torchvision.utils.make_grid(images))
 
-# Creación del modelo
-import torch.nn as nn # Módulo están los modelos de red
-import torch.nn.functional as F # Módulo donde están las funciones de activación
-
+# Creación del modelo DL
 class CNN(nn.Module):
   def __init__(self):
     super(CNN, self).__init__()
@@ -168,7 +128,6 @@ learning_rate = 0.001
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 #Optimizador
-
 model = CNN().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -217,18 +176,17 @@ for epoch in range(1, num_epochs+1):
 
     valid_loss += loss.item() * data.size(0)
 
-  # Calculo el promedio de las perdidas
+  # Calculo el promedio de Loss
   train_loss = train_loss/len(train_loader.sampler)
   valid_loss = valid_loss/len(valid_loader.sampler)
 
   train_losses.append(train_loss)
   valid_losses.append(valid_loss)
 
-  # Imprimimos las perdidas
+  # Imprimimos Loss (Pérdidas)
   print('Epoch: {} \tPerdida de entrenamiento: {:.6f} \tPerdida de Validacion: {:.6f}'.format(epoch, train_loss, valid_loss))
   
-  # Etapa de Pruebas
-  
+# Etapa de Pruebas 
 # Probando el modelo
 model.eval()  # it-disables-dropout
 with torch.no_grad():
@@ -244,15 +202,16 @@ with torch.no_grad():
           
     print('Test Accuracy of the model: {} %'.format(100 * correct / total))
 
-# Guardar
+# Guardar datos
 torch.save(model.state_dict(), 'model.ckpt')
 
 # Graficamos los resultados
 %matplotlib inline
 %config InlineBackend.figure_format = 'retina'
 
-plt.plot(train_losses, label='Training loss')
-plt.plot(valid_losses, label='Validation loss')
+plt.plot(train_losses, label='Training loss', 'r')
+plt.plot(valid_losses, label='Validation loss', 'b')
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.legend(frameon=False)
+plt.show()
